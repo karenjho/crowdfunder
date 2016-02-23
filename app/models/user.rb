1a797912931cabdad5b9f36ec259e5c95436aa01
Your Name <sylviapereira@gmail.com>
2016-02-23 17:12:54 -0500

    add validatiosn to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   has_many :pledges
   has_many :created_projects, class_name: "Project", foreign_key: "creator_id"
 
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :password, length: { in: 3..20 }
+
 end
