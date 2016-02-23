9dbdc0e7eb6888bd3802e8dec5fbb3dddc1e3295
sophiaxhe <sophiahxx@gmail.com>
2016-02-23 15:45:22 -0500

    added failed socery

---
+++ db/migrate/20160223165752_sorcery_core.rb
@@ -0,0 +1,7 @@
+class SorceryCore < ActiveRecord::Migration
+  def change
+    add_column :users, :crypted_password, :string
+    add_column :users, :salt, :string
+    remove_column :users, :password_digest, :string
+  end
+end
