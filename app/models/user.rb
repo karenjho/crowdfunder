213ce9865d3fe421e38a306c668c6f8b2138d3c1
stefanovtamara <stefanov.tamara@gmail.com>
2016-02-23 16:53:04 -0500

    make user session controller add validations etc

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   has_many :pledges
   has_many :created_projects, class_name: "Project", foreign_key: "creator_id"
 
+  validates :password, length: {minimum: 5}
+  validates :passsword, confirmation: true
+  validates :password_confirmation, presence: true
 end
