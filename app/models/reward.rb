01783b293c824c16b21e06cdd272df89d1542635
karenjho <hokarenj@gmail.com>
2016-02-25 16:04:14 -0500

    Create a dummy reward every time a project is created; also logout is delete method

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -2,4 +2,13 @@ class Reward < ActiveRecord::Base
   belongs_to :project
 
   has_many :pledges
+
+  def self.dummy_attrs
+    {
+      threshold: 0,
+      quantity: 9999,
+      name: "Best reward ever!",
+      description: "Our undying gratitude."
+    }
+  end
 end
