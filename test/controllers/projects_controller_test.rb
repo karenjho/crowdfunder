8e6194c9b138460ce964c10b2799a7b32db7b5ac
karenjho <hokarenj@gmail.com>
2016-02-26 15:45:47 -0500

    Write test to see if new project is created with dummy reward

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class ProjectsControllerTest < ActionController::TestCase
+
+  setup do
+    @user = FactoryGirl.create(:user)
+    # login method with sorcery? look up sorcery testing for log in/session creation
+  end
   # test "should get index" do
   #   get :index
   #   assert_response :success
@@ -11,10 +16,15 @@ class ProjectsControllerTest < ActionController::TestCase
   #   assert_response :success
   # end
   #
-  # test "should get new" do
-  #   get :new
-  #   assert_response :success
+  # test "post create fails with empty params" do
+  #   post :create, params: {}
+  #   assert_response :failure
   # end
+
+  test "post create adds reward with threshold 1" do
+    post :create, params: FactoryGirl.attributes_for(:project)
+    assert_equal( Project.last.rewards.first.threshold, 1 )
+  end
   #
   # test "should get edit" do
   #   get :edit
